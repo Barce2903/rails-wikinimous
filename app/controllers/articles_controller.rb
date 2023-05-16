fdefff385860faf5ec9fc55b476e448b36dbb5f1
Michael Barcellini <michael.barcellini@gmail.com>
2023-05-16 10:00:54 +0200

    index done

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,29 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
