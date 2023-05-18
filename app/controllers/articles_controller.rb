4af52670ae5eacfeec9c010c9c0f4b46eb0ee184
Michael Barcellini <michael.barcellini@gmail.com>
2023-05-18 21:54:58 +0200

    Exercice done, form css remain to do

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,42 @@
 class ArticlesController < ApplicationController
+  before_action :get_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-
+    @article = Article.update(params_article)
+    redirect_to task_path(@task)
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
-
+    @article = Article.new(params_article)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
 
+  private
+
+  def get_article
+    @article = Article.find(params[:id])
+  end
+
+  def params_article
+    params.require(:article).permit(:title, :content)
   end
 end
